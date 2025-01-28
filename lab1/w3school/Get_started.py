a76e34fc2c642ce0fcc6af38abcbdbfdcbf8e7b2
Azeke <karshygaaza@gmail.com>
2025-01-28 13:40:26 +0500

    Example,ex

---
+++ lab1/w3school/Get_started.py
@@ -0,0 +1,8 @@
+import sys 
+
+print(sys.version)
+
+#Whenever you are done in the python command line, you can simply type the following to quit the python command line interface:
+exit()
+
+python --version
