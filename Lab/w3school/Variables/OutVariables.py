c19f060aa70eb13898f66cae8d8e09f290467655
Azeke <karshygaaza@gmail.com>
2025-01-28 14:46:16 +0500

    rename

---
+++ Lab/w3school/Variables/OutVariables.py
@@ -0,0 +1,20 @@
+x = "Python is awesome"
+print(x)
+
+a = "Python"
+b = "is"
+c = "awesome"
+print(a, b, c)
+
+m = "Python "
+n = "is "
+l = "awesome"
+print(m + n + l)
+
+t = 5
+k = 10
+print(t + k)
+
+x = 5
+y = "John"
+print(x, y)
