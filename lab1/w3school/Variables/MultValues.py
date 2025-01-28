9055f6b21204f180aa9c97e6c61ca14ed4db05ec
Azeke <karshygaaza@gmail.com>
2025-01-28 14:44:06 +0500

    Examples

---
+++ lab1/w3school/Variables/MultValues.py
@@ -0,0 +1,15 @@
+a, b, c = "Orange", "Banana", "Cherry"
+print(a)
+print(b)
+print(c)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+m, n, l = fruits
+print(m)
+print(n)
+print(l)
