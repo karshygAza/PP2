10a51286acbb3c4a77a6d43f9bd6eac3af18b530
Azeke <karshygaaza@gmail.com>
2025-01-29 10:20:56 +0500

    delete

--- lab/lab1/w3school/Get_started.py
+++
@@ -1,8 +0,0 @@
-import sys 
-
-print(sys.version)
-
-#Whenever you are done in the python command line, you can simply type the following to quit the python command line interface:
-exit()
-
-python --version
