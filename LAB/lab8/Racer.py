e5d56ddbdb9bcf46afd16c89e512c456887cc937
Azeke <karshygaaza@gmail.com>
2025-04-01 00:43:24 +0500

    add comments

--- LAB/lab8/Racer.py
+++ LAB/lab8/Racer.py
@@ -4,31 +4,38 @@ import random, time
 
 pygame.init()
 
+# Устанавливаем параметры игры
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
+# Определяем цвета
 blue  = (0, 0, 255)
 red   = (255, 0, 0)
 green = (0, 255, 0)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+# Размеры экрана и начальные параметры игры
 screen_height = 600
 screen_width = 400
 speed = 5
 score = 0
 coins_collected = 0
 
+# Шрифты для текста
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, black)
 
+# Загрузка фона
 background = pygame.image.load("/Users/azamat/Documents/GitHub/PP2/LAB/lab8/AnimatedStreet.png")
 
+# Настройка экрана
 displaysurf = pygame.display.set_mode((screen_width, screen_height))
 displaysurf.fill(white)
 pygame.display.set_caption("Racer")
 
+# Класс для врага
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -47,6 +54,7 @@ class Enemy(pygame.sprite.Sprite):
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
+# Класс для игрока
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -64,6 +72,7 @@ class Player(pygame.sprite.Sprite):
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(5, 0)
 
+# Класс для монет
 class Coin(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -83,9 +92,11 @@ class Coin(pygame.sprite.Sprite):
         surface.blit(self.image, self.rect)
         
         
+# Создание объектов игрока и врага
 P1 = Player()
 E1 = Enemy()
 
+# Группы спрайтов для упрощения работы с ними
 enemies = pygame.sprite.Group()
 enemies.add(E1)
 all_sprites = pygame.sprite.Group()
@@ -93,59 +104,68 @@ all_sprites.add(P1)
 all_sprites.add(E1)
 coins = pygame.sprite.Group()
 
+# Таймер для увеличения скорости игры
 inc_speed = pygame.USEREVENT + 1
 pygame.time.set_timer(inc_speed, 1000)
 
+# Загрузка музыки
 pygame.mixer.music.load('/Users/azamat/Documents/GitHub/PP2/LAB/lab8/background.wav')
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.35)
 
+# Основной игровой цикл
 while True:
     for event in pygame.event.get():
-        if event.type == inc_speed:
+        if event.type == inc_speed:  # Увеличиваем скорость каждую секунду
             speed += 0.5
-        if event.type == QUIT:
+        if event.type == QUIT:  # Выход из игры
             pygame.quit()
             sys.exit()
             
-    displaysurf.blit(background, (0, 0))
+    displaysurf.blit(background, (0, 0))  # Рисуем фон
     
+    # Отображаем счёт и количество собранных монет
     scores = font_small.render(str(score), True, black)
     displaysurf.blit(scores, (10, 10))
     
     coins_display = font_small.render(str(coins_collected), True, black)
     displaysurf.blit(coins_display, (screen_width - 100, 10))
     
+    # Иногда создаём новую монету
     if random.random() < 0.005:
         new_coin = Coin()
         coins.add(new_coin)
         all_sprites.add(new_coin)
         
+    # Перемещаем и рисуем монеты
     for coin in coins:
         coin.move()
         coin.draw(displaysurf)        
     
+    # Перемещаем игрока и врагов
     for entity in all_sprites:
         entity.move()
         displaysurf.blit(entity.image, entity.rect)
         
+    # Проверка на сбор монет
     collected_coins = pygame.sprite.spritecollide(P1, coins, True)
     for _ in collected_coins:
         coins_collected += 1
         
+    # Проверка на столкновение с врагом
     if pygame.sprite.spritecollideany(P1, enemies):
-        pygame.mixer.Sound('/Users/azamat/Documents/GitHub/PP2/LAB/lab8/crash.wav').play()
+        pygame.mixer.Sound('/Users/azamat/Documents/GitHub/PP2/LAB/lab8/crash.wav').play()  # Звук столкновения
         time.sleep(1)
 
-        displaysurf.fill(red)
+        displaysurf.fill(red)  # Экран краснеет после столкновения
         displaysurf.blit(game_over, (30, 250))
 
         pygame.display.update()
-        for entity in all_sprites:
+        for entity in all_sprites:  # Удаляем все объекты
             entity.kill()
         time.sleep(2)
         pygame.quit()
-        sys.exit()         
+        sys.exit()  # Выход из игры
 
-    pygame.display.update()
-    FramePerSec.tick(FPS)
+    pygame.display.update()  # Обновляем экран
+    FramePerSec.tick(FPS)  # Ограничиваем частоту кадров
