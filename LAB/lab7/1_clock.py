c066f29f3eb8296296ee430d6e4af86651400974
Azeke <karshygaaza@gmail.com>
2025-03-31 23:07:35 +0500

    Add new image and finish

--- LAB/lab7/1_clock.py
+++ LAB/lab7/1_clock.py
@@ -7,12 +7,12 @@ pygame.init()
 screen = pygame.display.set_mode((900, 900))
 pygame.display.set_caption('Mickey clock')
 
-mickey = pygame.image.load('/Users/azamat/Documents/GitHub/PP2/LAB/lab7/mickeyclock.jpeg')
+mickey = pygame.image.load('/Users/azamat/Documents/GitHub/PP2/LAB/lab7/clock.png')
 right_hand = pygame.image.load('/Users/azamat/Documents/GitHub/PP2/LAB/lab7/right_hand.png')
 left_hand = pygame.image.load('/Users/azamat/Documents/GitHub/PP2/LAB/lab7/left_hand.png')
 
-right_hand = pygame.transform.scale(right_hand, (320, 370))
-left_hand = pygame.transform.scale(left_hand, (510, 355))
+right_hand = pygame.transform.scale(right_hand, (1400, 1050))
+left_hand = pygame.transform.scale(left_hand, (63, 1050))
 
 center_x = 450
 center_y = 450
@@ -29,7 +29,7 @@ while running:
     min_angle = math.radians(6 * minute - 90)
     
     right_hand_rotate = pygame.transform.rotate(right_hand, -(minute * 6))
-    right_hand_rect = right_hand_rotate.get_rect(center=(550,450))
+    right_hand_rect = right_hand_rotate.get_rect(center=(center_x, center_y))
     
     left_hand_rotate = pygame.transform.rotate(left_hand, -(sec * 6))
     left_hand_rect = left_hand_rotate.get_rect(center=(center_x, center_y))
